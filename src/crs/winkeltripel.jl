6ded6eed1476711b3602b2c77707798bc37c8ebd
Elias Carvalho <eliascarvdev@gmail.com>
2024-02-27 13:25:35 -0300

    Add 'Winkel' inverse

--- src/crs/winkeltripel.jl
+++ src/crs/winkeltripel.jl
@@ -39,6 +39,14 @@ const WinkelTripel{Datum} = Winkel{50.467u"°",Datum}
 # CONVERSIONS
 # ------------
 
+# Adapted from PROJ coordinate transformation software
+# Initial PROJ 4.3 public domain code was put as Frank Warmerdam as copyright
+# holder, but he didn't mean to imply he did the work. Essentially all work was
+# done by Gerald Evenden.
+
+# reference code: https://github.com/OSGeo/PROJ/blob/master/src/projections/aitoff.cpp
+# reference formula: https://en.wikipedia.org/wiki/Winkel_tripel_projection
+
 function formulas(::Type{<:Winkel{lat₁,Datum}}, ::Type{T}) where {lat₁,Datum,T}
   ϕ₁ = T(ustrip(deg2rad(lat₁)))
 
@@ -52,4 +60,19 @@ function formulas(::Type{<:Winkel{lat₁,Datum}}, ::Type{T}) where {lat₁,Datum
   fy(λ, ϕ) = (ϕ + sin(ϕ) / sincα(λ, ϕ)) / 2
 
   fx, fy
+end
+
+function Base.convert(::Type{LatLon{Datum}}, coords::C) where {lat₁,Datum,C<:Winkel{lat₁,Datum}}
+  T = numtype(coords.x)
+  a = numconvert(T, majoraxis(ellipsoid(Datum)))
+  x = coords.x / a
+  y = coords.y / a
+  tol = atol(T)
+  λ, ϕ = if abs(x) < tol && abs(y) < tol
+    zero(T), zero(T)
+  else
+    fx, fy = formulas(C, T)
+    projinv(fx, fy, x, y, x, y; tol)
+  end
+  LatLon{Datum}(rad2deg(ϕ) * u"°", rad2deg(λ) * u"°")
 end
