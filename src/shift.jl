560f4f87a4353b9c5305386a02eed0f9dfd10448
Elias Carvalho <eliascarvdev@gmail.com>
2024-04-12 14:22:55 -0300

    'Shifted' --> 'ShiftedCRS'

--- src/shift.jl
+++ src/shift.jl
@@ -8,7 +8,7 @@
 Shifts the `CRS` with given longitude origin `lonₒ` in degrees, false easting `xₒ`
 and false northing `yₒ` in meters.
 """
-shift(CRS::Type{<:Projected}; lonₒ=0.0u"°", xₒ=0.0u"m", yₒ=0.0u"m") = Shifted{CRS,lonₒ,xₒ,yₒ}
+shift(CRS::Type{<:Projected}; lonₒ=0.0u"°", xₒ=0.0u"m", yₒ=0.0u"m") = ShiftedCRS{CRS,lonₒ,xₒ,yₒ}
 
 """
     CoordRefSystems.shift(Datum, epoch)
