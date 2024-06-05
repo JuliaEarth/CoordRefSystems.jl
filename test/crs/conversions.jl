3d81bc2ee92afe8c9d434a256885dc200ccb78f6
Elias Carvalho <eliascarvdev@gmail.com>
2024-06-05 13:28:53 -0300

    Add more tests

--- test/crs/conversions.jl
+++ test/crs/conversions.jl
@@ -193,6 +193,10 @@
       c2 = convert(Cartesian{WGS84Latest}, c1)
       @test c1 === c2
 
+      c1 = Cartesian{WGS84Latest}(T(0), T(0))
+      c2 = convert(Cartesian{WGS84Latest}, c1)
+      @test c1 === c2
+
       c1 = Cartesian{WGS84Latest}(T(0), T(0), T(0))
       c2 = Cartesian{ITRF{2008}}(T(0), T(0), T(0))
       @inferred convert(Cartesian{ITRF{2008}}, c1)
