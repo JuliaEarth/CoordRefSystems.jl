b3f5043f28ca90dd23a4b7c8d12315f4c883b646
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-06-26 16:07:02 -0300

    Add 'constructor' function (#90)

--- src/crs/projected/webmercator.jl
+++ src/crs/projected/webmercator.jl
@@ -36,7 +36,9 @@ WebMercator(args...) = WebMercator{WGS84Latest}(args...)
 Base.convert(::Type{WebMercator{Datum,M}}, coords::WebMercator{Datum}) where {Datum,M} =
   WebMercator{Datum,M}(coords.x, coords.y)
 
-lentype(::Type{WebMercator{Datum,M}}) where {Datum,M} = M
+lentype(::Type{<:WebMercator{Datum,M}}) where {Datum,M} = M
+
+constructor(::Type{<:WebMercator{Datum}}) where {Datum} = WebMercator{Datum}
 
 ==(coords₁::WebMercator{Datum}, coords₂::WebMercator{Datum}) where {Datum} =
   coords₁.x == coords₂.x && coords₁.y == coords₂.y
