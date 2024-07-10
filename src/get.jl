810a208b9dd44308c08e5bfa430244b5f8a20819
Júlio Hoffimann <julio.hoffimann@gmail.com>
2024-07-10 08:27:23 -0300

    Add EPSG{10176}

--- src/get.jl
+++ src/get.jl
@@ -30,6 +30,7 @@ get(::Type{EPSG{3395}}) = Mercator{WGS84Latest}
 get(::Type{EPSG{3857}}) = WebMercator{WGS84Latest}
 get(::Type{EPSG{4326}}) = LatLon{WGS84Latest}
 get(::Type{EPSG{9988}}) = Cartesian{AmericaS2000,3}
+get(::Type{EPSG{10176}}) = Cartesian{IGS20,3}
 get(::Type{EPSG{32633}}) = UTM{North,33,WGS84Latest}
 get(::Type{EPSG{32662}}) = PlateCarree{WGS84Latest}
 get(::Type{ESRI{54017}}) = Behrmann{WGS84Latest}
