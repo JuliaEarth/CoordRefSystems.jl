49ddc0e7079879b9089024c9b26c4b09a6625a69
Elias Carvalho <eliascarvdev@gmail.com>
2024-07-11 10:01:00 -0300

    Add 'get' methods

--- test/get.jl
+++ test/get.jl
@@ -3,9 +3,11 @@
   @test CoordRefSystems.get(EPSG{3395}) === Mercator{WGS84Latest}
   @test CoordRefSystems.get(EPSG{3857}) === WebMercator{WGS84Latest}
   @test CoordRefSystems.get(EPSG{4208}) === LatLon{Aratu}
+  @test CoordRefSystems.get(EPSG{4618}) === LatLon{SAD69}
   @test CoordRefSystems.get(EPSG{4326}) === LatLon{WGS84Latest}
   @test CoordRefSystems.get(EPSG{4988}) === Cartesian{CoordRefSystems.shift(ITRF{2000}, 2000.4),3}
   @test CoordRefSystems.get(EPSG{4989}) === LatLonAlt{CoordRefSystems.shift(ITRF{2000}, 2000.4)}
+  @test CoordRefSystems.get(EPSG{5527}) === LatLon{SAD96}
   @test CoordRefSystems.get(EPSG{9988}) === Cartesian{ITRF{2020},3}
   @test CoordRefSystems.get(EPSG{10176}) === Cartesian{IGS20,3}
   @test CoordRefSystems.get(EPSG{32633}) === UTM{North,33,WGS84Latest}
