8ec56297fed321d7e64d1f979ee8dc4c46dda79a
Elias Carvalho <eliascarvdev@gmail.com>
2024-07-12 08:30:14 -0300

    Add 'reversible' macro

--- src/geoctransforms.jl
+++ src/geoctransforms.jl
@@ -8,6 +8,16 @@ struct Reverse{T<:GeocentricTransform} <: GeocentricTransform
   transform::T
 end
 
+Base.getproperty(transform::Reverse, name::Symbol) = getproperty(getfield(transform, :transform), name)
+
+macro reversible(Datum₁, Datum₂, transform)
+  expr = quote
+    geoctransform(::Type{<:$Datum₁}, ::Type{<:$Datum₂}) = $transform
+    geoctransform(D₂::Type{<:$Datum₂}, D₁::Type{<:$Datum₁}) = Reverse(geoctransform(D₁, D₂))
+  end
+  esc(expr)
+end
+
 function geoctransform end
 
 function geocapply end
@@ -30,36 +40,47 @@ include("geoctransforms/geochelmert.jl")
 # EPSG Database: https://epsg.org/search/by-name
 # PROJ source code: https://github.com/OSGeo/PROJ/blob/master/src/datums.cpp
 
-geoctransform(::Type{<:WGS84}, ::Type{SAD69}) = GeocentricTranslation(δx=-57.0, δy=1.0, δz=-41.0)
+@reversible WGS84 SAD69 GeocentricTranslation(δx=-57.0, δy=1.0, δz=-41.0)
 
-geoctransform(::Type{WGS84{2296}}, ::Type{ITRF{2020}}) = GeocentricIdentity()
+@reversible WGS84{2296} ITRF{2020} GeocentricIdentity()
 
-geoctransform(::Type{WGS84{2139}}, ::Type{ITRF{2014}}) = GeocentricIdentity()
+@reversible WGS84{2139} ITRF{2014} GeocentricIdentity()
 
-geoctransform(::Type{WGS84{1762}}, ::Type{ITRF{2008}}) = GeocentricIdentity()
+@reversible WGS84{1762} ITRF{2008} GeocentricIdentity()
 
-geoctransform(::Type{GGRS87}, ::Type{<:WGS84}) = GeocentricHelmert(δx=-199.87, δy=74.79, δz=246.62)
+@reversible GGRS87 WGS84 GeocentricHelmert(δx=-199.87, δy=74.79, δz=246.62)
 
-geoctransform(::Type{NAD83}, ::Type{<:WGS84}) = GeocentricIdentity()
+@reversible NAD83 WGS84 GeocentricIdentity()
 
-geoctransform(::Type{Potsdam}, ::Type{<:WGS84}) =
-  GeocentricHelmert(δx=598.1, δy=73.7, δz=418.2, θx=0.202, θy=0.045, θz=-2.455, s=6.7)
+@reversible Potsdam WGS84 GeocentricHelmert(δx=598.1, δy=73.7, δz=418.2, θx=0.202, θy=0.045, θz=-2.455, s=6.7)
 
-geoctransform(::Type{Carthage}, ::Type{<:WGS84}) = GeocentricHelmert(δx=-263.0, δy=6.0, δz=431.0)
+@reversible Carthage WGS84 GeocentricHelmert(δx=-263.0, δy=6.0, δz=431.0)
 
-geoctransform(::Type{Hermannskogel}, ::Type{<:WGS84}) =
-  GeocentricHelmert(δx=577.326, δy=90.129, δz=463.919, θx=5.137, θy=1.474, θz=5.297, s=2.4232)
+@reversible Hermannskogel WGS84 GeocentricHelmert(
+  δx=577.326,
+  δy=90.129,
+  δz=463.919,
+  θx=5.137,
+  θy=1.474,
+  θz=5.297,
+  s=2.4232
+)
 
-geoctransform(::Type{Ire65}, ::Type{<:WGS84}) =
-  GeocentricHelmert(δx=482.530, δy=-130.596, δz=564.557, θx=-1.042, θy=-0.214, θz=-0.631, s=8.15)
+@reversible Ire65 WGS84 GeocentricHelmert(δx=482.530, δy=-130.596, δz=564.557, θx=-1.042, θy=-0.214, θz=-0.631, s=8.15)
 
-geoctransform(::Type{NZGD1949}, ::Type{<:WGS84}) =
-  GeocentricHelmert(δx=59.47, δy=-5.04, δz=187.44, θx=0.47, θy=-0.1, θz=1.024, s=-4.5993)
+@reversible NZGD1949 WGS84 GeocentricHelmert(δx=59.47, δy=-5.04, δz=187.44, θx=0.47, θy=-0.1, θz=1.024, s=-4.5993)
 
-geoctransform(::Type{OSGB36}, ::Type{<:WGS84}) =
-  GeocentricHelmert(δx=446.448, δy=-125.157, δz=542.060, θx=0.1502, θy=0.2470, θz=0.8421, s=-20.4894)
+@reversible OSGB36 WGS84 GeocentricHelmert(
+  δx=446.448,
+  δy=-125.157,
+  δz=542.060,
+  θx=0.1502,
+  θy=0.2470,
+  θz=0.8421,
+  s=-20.4894
+)
 
-geoctransform(::Type{ITRF{2008}}, ::Type{ITRF{2020}}) = timedephelmert(
+@reversible ITRF{2008} ITRF{2020} timedephelmert(
   ITRF{2008},
   ITRF{2020},
   δx=-0.2e-3,
