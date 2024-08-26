e0044d7954c5ae6809a78efb0ef4436ea1521c3b
Elias Carvalho <eliascarvdev@gmail.com>
2024-08-26 16:18:15 -0300

    More adjustments

--- src/utm.jl
+++ src/utm.jl
@@ -26,9 +26,12 @@ abstract type South <: Hemisphere end
 """
     utm(H::Type{<:Hemisphere}, zone, datum=WGS84Latest)
 
-UTM (Universal Transverse Mercator) CRS in hemisphere `H` with `zone` (1 ≤ Zone ≤ 60) and a given `datum`.
+UTM (Universal Transverse Mercator) CRS in hemisphere `H` with `zone` (1 ≤ zone ≤ 60) and a given `datum`.
 """
 function utm(H::Type{<:Hemisphere}, zone, datum=WGS84Latest)
+  if !(1 ≤ zone ≤ 60)
+    throw(ArgumentError("the UTM zone must be an integer between 1 and 60"))
+  end
   k₀ = 0.9996
   latₒ = 0.0°
   lonₒ = (6 * zone - 183) * °
