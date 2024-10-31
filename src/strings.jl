6db05208a65db5a23eef217626aae9ca2ccf50f4
Jeffrey Chandler <jeffrey.chandler435@gmail.com>
2024-10-31 16:11:39 -0600

    Adding EPSG:2180 for usage in vector-benchmark tests (#188)

--- src/strings.jl
+++ src/strings.jl
@@ -7,6 +7,7 @@
 # dictionary in **alphabetical order**
 const esriid2code = Dict(
   "British_National_Grid" => EPSG{27700},
+  "ETRF2000-PL_CS92" => EPSG{2180},
   "GCS_MAGNA" => EPSG{4686},
   "GCS_North_American_1983" => EPSG{4269},
   "GCS_SAD_1969_96" => EPSG{5527},
