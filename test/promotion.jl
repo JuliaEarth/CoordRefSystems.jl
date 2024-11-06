d30aa47446dcdcde834a6478f6595ec4bb2be8ad
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-11-05 21:21:55 -0300

    Add promotion methods for `Basic` CRSs (#189)
    * Add promotion methods for 'Basic' CRSs
    
    * Add tests
    
    * Update order

--- test/promotion.jl
+++ test/promotion.jl
@@ -52,6 +52,45 @@
   @test allequal(CoordRefSystems.mactype.(cs))
   @test CoordRefSystems.mactype(first(cs)) == T
 
+  # Polar and Polar
+  c1 = Polar(T(1), T(1))
+  c2 = Polar(1.0, 1.0)
+  c3 = Polar(1.0f0, 1.0f0)
+  cs = promote(c1, c2)
+  @test all(c -> c isa Polar, cs)
+  @test allequal(CoordRefSystems.mactype.(cs))
+  @test CoordRefSystems.mactype(first(cs)) == Float64
+  cs = promote(c1, c3)
+  @test all(c -> c isa Polar, cs)
+  @test allequal(CoordRefSystems.mactype.(cs))
+  @test CoordRefSystems.mactype(first(cs)) == T
+
+  # Cylindrical and Cylindrical
+  c1 = Cylindrical(T(1), T(1), T(1))
+  c2 = Cylindrical(1.0, 1.0, 1.0)
+  c3 = Cylindrical(1.0f0, 1.0f0, 1.0f0)
+  cs = promote(c1, c2)
+  @test all(c -> c isa Cylindrical, cs)
+  @test allequal(CoordRefSystems.mactype.(cs))
+  @test CoordRefSystems.mactype(first(cs)) == Float64
+  cs = promote(c1, c3)
+  @test all(c -> c isa Cylindrical, cs)
+  @test allequal(CoordRefSystems.mactype.(cs))
+  @test CoordRefSystems.mactype(first(cs)) == T
+
+  # Spherical and Spherical
+  c1 = Spherical(T(1), T(1), T(1))
+  c2 = Spherical(1.0, 1.0, 1.0)
+  c3 = Spherical(1.0f0, 1.0f0, 1.0f0)
+  cs = promote(c1, c2)
+  @test all(c -> c isa Spherical, cs)
+  @test allequal(CoordRefSystems.mactype.(cs))
+  @test CoordRefSystems.mactype(first(cs)) == Float64
+  cs = promote(c1, c3)
+  @test all(c -> c isa Spherical, cs)
+  @test allequal(CoordRefSystems.mactype.(cs))
+  @test CoordRefSystems.mactype(first(cs)) == T
+
   # Cartesian2D and Cartesian2D
   c1 = Cartesian(T(1), T(1))
   c2 = Cartesian(1.0, 1.0)
@@ -87,9 +126,34 @@
   @test allequal(CoordRefSystems.mactype.(cs))
   @test CoordRefSystems.mactype(first(cs)) == T
 
+  # Cartesian2D and Polar
+  c1 = Cartesian(T(1), T(1))
+  c2 = convert(Polar, c1)
+  cs = promote(c1, c2)
+  @test all(c -> c isa Cartesian2D, cs)
+  @test allequal(CoordRefSystems.mactype.(cs))
+  @test CoordRefSystems.mactype(first(cs)) == T
+
+  # Cartesian3D and Cylindrical
+  c1 = Cartesian(T(1), T(1), T(1))
+  c2 = convert(Cylindrical, c1)
+  cs = promote(c1, c2)
+  @test all(c -> c isa Cartesian3D, cs)
+  @test allequal(CoordRefSystems.mactype.(cs))
+  @test CoordRefSystems.mactype(first(cs)) == T
+
+  # Cartesian3D and Spherical
+  c1 = Cartesian(T(1), T(1), T(1))
+  c2 = convert(Spherical, c1)
+  cs = promote(c1, c2)
+  @test all(c -> c isa Cartesian3D, cs)
+  @test allequal(CoordRefSystems.mactype.(cs))
+  @test CoordRefSystems.mactype(first(cs)) == T
+
   # Cartesian3D and LatLon
   c1 = Cartesian{WGS84Latest}(T(6378137.0), T(0), T(0))
   c2 = convert(LatLon, c1)
+  cs = promote(c1, c2)
   @test all(c -> c isa Cartesian3D, cs)
   @test allequal(CoordRefSystems.mactype.(cs))
   @test CoordRefSystems.mactype(first(cs)) == T
