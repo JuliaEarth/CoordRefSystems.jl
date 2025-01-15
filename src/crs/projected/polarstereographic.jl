9f388093fca8816610752b07e5427c6ba26ebd0f
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2025-01-15 17:13:20 -0500

    Update test info with correct Shift values

--- src/crs/projected/polarstereographic.jl
+++ src/crs/projected/polarstereographic.jl
@@ -72,16 +72,17 @@ const EPSG3031{Shift} = PolarStereographicB{-71°,0°,WGS84,Shift}
 
 #=
 Tested against the manual with:
-convert(CoordRefSystems.PolarStereographicB{-71, 70}, LatLon(-75,120))
-┌ Info: Values
+julia> convert(CoordRefSystems.PolarStereographicB{-71, 70, WGS84Latest, CoordRefSystems.Shift(xₒ=6e6m, yₒ=6e6m)}, LatLon(-75,120))
+┌ Debug: Values
 │   tF = 0.16840732452916302
 │   mF = 0.32654678137878085
 │   kO = 0.9727690128917972
 │   t = 0.13250834771195819
-└   ρ = 0.25693760394410403
-PolarStereographicB{WGS84Latest} coordinates
-├─ x: 1.2553807932583862e6 m
-└─ y: 1.0533895606101535e6 m
+│   ρ = 0.25693760394410403
+└ @ CoordRefSystems ~/.julia/dev/CoordRefSystems/src/crs/projected/polarstereographic.jl:120
+PolarStereographicB{WGS84Latest} coordinates with lonₒ: 0.0°, xₒ: 6.0e6 m, yₒ: 6.0e6 m
+├─ x: 7.255380793258386e6 m
+└─ y: 7.053389560610154e6 m
 =#
 # TODO: can FE and FN (false east and north) be removed by composing with a shift?
 function formulas(::Type{<:PolarStereographicB{latF,lngₒ,Datum}}, ::Type{T}) where {latF,lngₒ,Datum,T}
@@ -148,7 +149,24 @@ end
 
 #=
 test with:
-epsg = CoordRefSystems.PolarStereographicB{-71, 0}(7255380.79-6e6, 7053389.56-6e6)
+julia> convert(LatLon, CoordRefSystems.PolarStereographicB{-71, 70, WGS84Latest, CoordRefSystems.Shift(xₒ=6e6m, yₒ=6e6m)}(7255380.79, 7053389.56))
+┌ Debug: Inputs
+│   x = 0.19682562321881766
+│   y = 0.16515630818215407
+│   E = 1.25538079e6
+│   N = 1.0533895599999996e6
+└ @ CoordRefSystems ~/.julia/dev/CoordRefSystems/src/crs/projected/polarstereographic.jl:165
+┌ Debug: Intermediates
+│   tF = 0.16840732452916302
+│   mF = 0.32654678137878085
+│   kO = 0.9727690128917972
+│   ρ′ = 1.6387832355189677e6
+│   t′ = 0.13250834747841927
+│   X = -1.3073145883173596
+└ @ CoordRefSystems ~/.julia/dev/CoordRefSystems/src/crs/projected/polarstereographic.jl:182
+GeodeticLatLon{WGS84Latest} coordinates
+├─ lat: -75.00000002614524°
+└─ lon: 119.9999999431146°
 =#
 function backward(::Type{<:PolarStereographicB{latF,lngₒ,Datum}}, x, y) where {latF,lngₒ,Datum}
   ϕF = oftype(x, ustrip(deg2rad(latF)))
