d54294be898db2bf3915d808d0af00e30ae2cfc5
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2025-01-21 13:00:15 -0500

    Remove old testing comments

--- src/crs/projected/polarstereographic.jl
+++ src/crs/projected/polarstereographic.jl
@@ -48,20 +48,6 @@ lentype(::Type{<:PolarStereographicB{lat₁,Datum,Shift,M}}) where {lat₁,Datum
 # CONVERSIONS
 # ------------
 
-#=
-Tested against the manual with:
-julia> convert(CoordRefSystems.PolarStereographicB{-71, 70, WGS84Latest, CoordRefSystems.Shift(xₒ=6e6m, yₒ=6e6m)}, LatLon(-75,120))
-┌ Debug: Values
-│   tF = 0.16840732452916302
-│   mF = 0.32654678137878085
-│   kO = 0.9727690128917972
-│   t = 0.13250834771195819
-│   ρ = 0.25693760394410403
-└ @ CoordRefSystems ~/.julia/dev/CoordRefSystems/src/crs/projected/polarstereographic.jl:120
-PolarStereographicB{WGS84Latest} coordinates with lonₒ: 0.0°, xₒ: 6.0e6 m, yₒ: 6.0e6 m
-├─ x: 7.255380793258386e6 m
-└─ y: 7.053389560610154e6 m
-=#
 function formulas(::Type{<:PolarStereographicB{lat₁,Datum}}, ::Type{T}) where {lat₁,Datum,T}
   ϕF = Float64(ustrip(deg2rad(Float64(lat₁))))
 
@@ -104,27 +90,6 @@ function formulas(::Type{<:PolarStereographicB{lat₁,Datum}}, ::Type{T}) where 
   fx, fy
 end
 
-#=
-test with:
-julia> convert(LatLon, CoordRefSystems.PolarStereographicB{-71, 70, WGS84Latest, CoordRefSystems.Shift(xₒ=6e6m, yₒ=6e6m)}(7255380.79, 7053389.56))
-┌ Debug: Inputs
-│   x = 0.19682562321881766
-│   y = 0.16515630818215407
-│   E = 1.25538079e6
-│   N = 1.0533895599999996e6
-└ @ CoordRefSystems ~/.julia/dev/CoordRefSystems/src/crs/projected/polarstereographic.jl:165
-┌ Debug: Intermediates
-│   tF = 0.16840732452916302
-│   mF = 0.32654678137878085
-│   kO = 0.9727690128917972
-│   ρ′ = 1.6387832355189677e6
-│   t′ = 0.13250834747841927
-│   X = -1.3073145883173596
-└ @ CoordRefSystems ~/.julia/dev/CoordRefSystems/src/crs/projected/polarstereographic.jl:182
-GeodeticLatLon{WGS84Latest} coordinates
-├─ lat: -75.00000002614524°
-└─ lon: 119.9999999431146°
-=#
 function backward(::Type{<:PolarStereographicB{lat₁,Datum}}, x, y) where {lat₁,Datum}
   T = typeof(x)
   ϕF = T(ustrip(deg2rad(lat₁)))
