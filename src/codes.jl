c901d77481f5d1f239fd61b58d5a083f4570dcac
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-03-21 07:38:13 -0300

    Improve error messages in wkt2

--- src/codes.jl
+++ src/codes.jl
@@ -29,24 +29,25 @@ ESRI dataset `code`. Codes can be searched at [epsg.io](https://epsg.io).
 abstract type ESRI{Code} <: CRSCode end
 
 function wkt2(::Type{EPSG{Code}}) where {Code}
-  datasetpath =  pkgdir(CoordRefSystems, "artifacts", "epsg-wkt2")
-  filepath = joinpath(datasetpath, "EPSG-CRS-$Code.wkt")
-  if !isfile(filepath)
+  epsg = joinpath(pkgdir(@__MODULE__), "artifacts", "epsg-wkt2")
+  file = joinpath(epsg, "EPSG-CRS-$Code.wkt")
+  if !isfile(file)
     throw(ArgumentError(
     """
     EPSG:$Code not found. This EPSG code may be deprecated.
     Supported and deprecated codes can be checked at https://epsg.io.
     Please reproject the data to a CRS supported by the latest release
-    of the EPSG dataset.
+    of the EPSG dataset: https://github.com/JuliaEarth/CoordRefSystems.jl/tree/main/artifacts/epsg-wkt2
     """))
   end
-  read(filepath, String)
+  read(file, String)
 end
 
 function wkt2(::Type{ESRI{Code}}) where {Code}
   throw(ArgumentError(
   """
   OGC WKT-CRS version 2 does not support ESRI:$Code.
-  Please reproject the data to a CRS with EPSG code.
+  Please reproject the data to a CRS with EPSG code:
+  https://github.com/JuliaEarth/CoordRefSystems.jl/blob/main/src/get.jl
   """))
 end
