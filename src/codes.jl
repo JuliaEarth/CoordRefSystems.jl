0de7d1a8d0effabe145a55a2467b7b204a927d78
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-03-21 07:17:43 -0300

    Move things around for better organization

--- src/codes.jl
+++ src/codes.jl
@@ -28,30 +28,6 @@ ESRI dataset `code`. Codes can be searched at [epsg.io](https://epsg.io).
 """
 abstract type ESRI{Code} <: CRSCode end
 
-"""
-    CoordRefSystems.code(coords)
-
-EPSG/ESRI code of given `coords`.
-
-For the inverse operation, see the [`CoordRefSystems.get`](@ref) function.
-"""
-code(coords::CRS) = code(typeof(coords))
-function code(crs::Type{<:CRS})
-  throw(ArgumentError("""
-  The provided CRS type `$crs` does not have an EPSG/ESRI code.
-  Please check https://github.com/JuliaEarth/CoordRefSystems.jl/blob/main/src/get.jl
-  """))
-end
-
-"""
-    CoordRefSystems.wkt2(coords)
-
-Well-Known-Text (WKT) representation of given `coords` compliant with the
-OGC WKT-CRS 2 standard: https://www.ogc.org/publications/standard/wkt-crs
-"""
-wkt2(coords::CRS) = wkt2(typeof(coords))
-wkt2(C::Type{<:CRS}) = wkt2(code(C))
-
 function wkt2(::Type{EPSG{Code}}) where {Code}
   datasetpath =  pkgdir(CoordRefSystems, "artifacts", "epsg-wkt2")
   filepath = joinpath(datasetpath, "EPSG-CRS-$Code.wkt")
