bc3a72f303e6190fcde932cf3c79e45682e5badd
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-05-19 16:00:26 -0300

    Refactor allapprox in tests

--- test/testutils.jl
+++ test/testutils.jl
@@ -1,23 +1,10 @@
-allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:CRS} =
-  all(ntuple(i -> isapprox(getfield(coords₁, i), getfield(coords₂, i); kwargs...), nfields(coords₁)))
-
-function allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:Cartesian2D}
-  isapprox(coords₁.x, coords₂.x, kwargs...) && isapprox(coords₁.y, coords₂.y, kwargs...)
-end
-
-function allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:Cartesian3D}
-  isapprox(coords₁.x, coords₂.x, kwargs...) &&
-    isapprox(coords₁.y, coords₂.y, kwargs...) &&
-    isapprox(coords₁.z, coords₂.z, kwargs...)
-end
+allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:CRS} = isapprox(coords₁, coords₂; kwargs...)
 
 allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:LatLon} =
   isapprox(coords₁.lat, coords₂.lat; kwargs...) && (
     isapprox(coords₁.lon, coords₂.lon; kwargs...) ||
-    (isapproxlon180(coords₁.lon; kwargs...) && isapprox(coords₁.lon, -coords₂.lon; kwargs...))
+    (isapprox(abs(coords₁.lon), 180°; kwargs...) && isapprox(coords₁.lon, -coords₂.lon; kwargs...))
   )
-
-isapproxlon180(lon; kwargs...) = isapprox(abs(lon), 180°; kwargs...)
 
 function isapproxtest2D(CRS)
   x = T(1) * m
