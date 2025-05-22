1673ce942668719a7c053eac786a7002c6853ec9
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-05-22 15:11:03 -0300

    Reduce diff by renaming isclose back to allapprox

--- test/testutils.jl
+++ test/testutils.jl
@@ -2,20 +2,20 @@
 # BASIC UTILITITIES
 # ------------------
 
-isclose(coords₁::C, coords₂::C; kwargs...) where {C<:CRS} =
+allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:CRS} =
   all(ntuple(i -> isapprox(getfield(coords₁, i), getfield(coords₂, i); kwargs...), nfields(coords₁)))
 
-function isclose(coords₁::C, coords₂::C; kwargs...) where {C<:Cartesian2D}
+function allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:Cartesian2D}
   isapprox(coords₁.x, coords₂.x, kwargs...) && isapprox(coords₁.y, coords₂.y, kwargs...)
 end
 
-function isclose(coords₁::C, coords₂::C; kwargs...) where {C<:Cartesian3D}
+function allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:Cartesian3D}
   isapprox(coords₁.x, coords₂.x, kwargs...) &&
     isapprox(coords₁.y, coords₂.y, kwargs...) &&
     isapprox(coords₁.z, coords₂.z, kwargs...)
 end
 
-isclose(coords₁::C, coords₂::C; kwargs...) where {C<:LatLon} =
+allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:LatLon} =
   isapprox(coords₁.lat, coords₂.lat; kwargs...) && (
     isapprox(coords₁.lon, coords₂.lon; kwargs...) ||
     (isapprox(abs(coords₁.lon), 180°; kwargs...) && isapprox(coords₁.lon, -coords₂.lon; kwargs...))
