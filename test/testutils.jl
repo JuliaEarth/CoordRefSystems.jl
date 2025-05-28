2b55ac7d34bc17f898bb580b4863a2b966bd3ddd
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-05-28 10:12:15 -0300

    Refactor tests

--- test/testutils.jl
+++ test/testutils.jl
@@ -2,18 +2,7 @@
 # BASIC UTILITITIES
 # ------------------
 
-allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:CRS} =
-  all(ntuple(i -> isapprox(getfield(coords₁, i), getfield(coords₂, i); kwargs...), nfields(coords₁)))
-
-function allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:Cartesian2D}
-  isapprox(coords₁.x, coords₂.x, kwargs...) && isapprox(coords₁.y, coords₂.y, kwargs...)
-end
-
-function allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:Cartesian3D}
-  isapprox(coords₁.x, coords₂.x, kwargs...) &&
-    isapprox(coords₁.y, coords₂.y, kwargs...) &&
-    isapprox(coords₁.z, coords₂.z, kwargs...)
-end
+allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:CRS} = isapprox(coords₁, coords₂; kwargs...)
 
 allapprox(coords₁::C, coords₂::C; kwargs...) where {C<:LatLon} =
   isapprox(coords₁.lat, coords₂.lat; kwargs...) && (
