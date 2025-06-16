f3dd131b00a31ff148ca6888f28a4f43d7289aae
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-06-16 11:02:18 -0300

    Add NAD27 <> WGS84 conversion (#302)
    * Add NAD27 <> WGS84 conversion
    
    * Update src/transforms.jl
    
    Co-authored-by: Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Elias Carvalho <73039601+eliascarv@users.noreply.github.com>

--- src/transforms.jl
+++ src/transforms.jl
@@ -39,6 +39,9 @@ include("transforms/sequential.jl")
 # https://epsg.org/transformation_1565/NZGD2000-to-WGS-84-1.html
 @identity NZGD2000 WGS84
 
+# https://epsg.org/transformation_1170/NAD27-to-WGS-84-1.html
+@geoctranslation NAD27 WGS84 (δx=-3.0, δy=142.0, δz=183.0)
+
 # https://epsg.org/transformation_1130/Carthage-to-WGS-84-1.html
 @geoctranslation Carthage WGS84 (δx=-263.0, δy=6.0, δz=431.0)
 
