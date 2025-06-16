675fa580c7f55315f38ec8390f04301e0bf97f9d
tcarion <tristancarion@gmail.com>
2025-06-16 10:53:22 +0200

    clarify comments

--- test/crs/fwdbwd.jl
+++ test/crs/fwdbwd.jl
@@ -44,6 +44,8 @@
       PRJ <: EqualEarth && abs(lat) == T(90) && continue
 
       # https://github.com/JuliaEarth/CoordRefSystems.jl/pull/264
+      # the LambertConic projection maps all values of the form (90, lon) to  (x, y)
+      # therefore we cannot recover the original lon value when lat=90
       PRJ <: LambertConic && lat == T(90) && continue
 
       ll = LatLon(lat, lon)
