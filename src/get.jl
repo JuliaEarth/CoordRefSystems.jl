c8d229cb914df5dc39337c7ee6c4f9c6ab5a43c2
Elias Carvalho <eliascarvdev@gmail.com>
2025-10-17 17:25:11 -0300

    Redesign the '@crscode' macro to support multiple codes for the same CRS

--- src/get.jl
+++ src/get.jl
@@ -27,14 +27,17 @@ function get(code::Type{<:CRSCode})
 end
 
 """
-    @crscode(Code, CRS)
+    @crscode CRS Code₁ ... Codeₙ
 
-Define both `get` and `code` functions for `Code` and `CRS`.
+Define the `get(Codeᵢ) -> CRS` function mapping all codes to `CRS`,
+and define the `code(CRS) -> Code₁` function using the first code.
 """
-macro crscode(Code, CRS)
+macro crscode(CRS, Codes...)
+  Code₁ = first(Codes)
+  getexprs = [:(get(::Type{$Code}) = $CRS) for Code in Codes]
   expr = quote
-    get(::Type{$Code}) = $CRS
-    code(::Type{<:$CRS}) = $Code
+    $(getexprs...)
+    code(::Type{<:$CRS}) = $Code₁
   end
   esc(expr)
 end
@@ -43,80 +46,80 @@ end
 # IMPLEMENTATIONS
 # ----------------
 
-@crscode EPSG{2039} shift(
+@crscode shift(
   TransverseMercator{1.0000067,31.734394°,Israel1993},
   lonₒ=35.204517°,
   xₒ=219529.584m,
   yₒ=626907.39m
-)
-@crscode EPSG{2157} shift(TransverseMercator{0.99982,53.5°,IRENET95}, lonₒ=-8.0°, xₒ=600000.0m, yₒ=750000.0m)
-@crscode EPSG{2180} shift(TransverseMercator{0.9993,0.0°,ETRF{2000}}, lonₒ=19.0°, xₒ=500000.0m, yₒ=-5300000.0m)
-@crscode EPSG{2193} shift(TransverseMercator{0.9996,0.0°,NZGD2000}, lonₒ=173.0°, xₒ=1600000.0m, yₒ=10000000.0m)
-@crscode EPSG{3035} shift(LambertAzimuthal{52.0°,ETRFLatest}, lonₒ=10.0°, xₒ=4321000.0m, yₒ=3210000.0m)
-@crscode EPSG{3310} shift(Albers{0.0°,34.0°,40.5°,NAD83}, lonₒ=-120.0°, yₒ=-4000000.0m)
-@crscode EPSG{3338} shift(Albers{50.0°,55.0°,65.0°,NAD83}, lonₒ=-154.0°)
-@crscode EPSG{3395} Mercator{WGS84Latest}
-@crscode EPSG{3857} WebMercator{WGS84Latest}
-@crscode EPSG{4171} LatLon{RGF93v1}
-@crscode EPSG{4207} LatLon{Lisbon1937}
-@crscode EPSG{4208} LatLon{Aratu}
-@crscode EPSG{4230} LatLon{ED50}
-@crscode EPSG{4231} LatLon{ED87}
-@crscode EPSG{4267} LatLon{NAD27}
-@crscode EPSG{4269} LatLon{NAD83}
-@crscode EPSG{4274} LatLon{Datum73}
-@crscode EPSG{4275} LatLon{NTF}
-@crscode EPSG{4277} LatLon{OSGB36}
-@crscode EPSG{4314} LatLon{DHDN}
-@crscode EPSG{4326} LatLon{WGS84Latest}
-@crscode EPSG{4618} LatLon{SAD69}
-@crscode EPSG{4659} LatLon{ISN93}
-@crscode EPSG{4666} LatLon{Lisbon1890}
-@crscode EPSG{4668} LatLon{ED79}
-@crscode EPSG{4674} LatLon{SIRGAS2000}
-@crscode EPSG{4745} LatLon{RD83}
-@crscode EPSG{4746} LatLon{PD83}
-@crscode EPSG{4988} Cartesian3D{shift(ITRF{2000}, 2000.4)}
-@crscode EPSG{4989} LatLonAlt{shift(ITRF{2000}, 2000.4)}
-@crscode EPSG{5070} shift(Albers{23.0°,29.5°,45.5°,NAD83}, lonₒ=-96.0°)
-@crscode EPSG{5324} LatLon{ISN2004}
-@crscode EPSG{5527} LatLon{SAD96}
-@crscode EPSG{8086} LatLon{ISN2016}
-@crscode EPSG{8232} LatLon{NAD83CSRS{1}}
-@crscode EPSG{8237} LatLon{NAD83CSRS{2}}
-@crscode EPSG{8240} LatLon{NAD83CSRS{3}}
-@crscode EPSG{8246} LatLon{NAD83CSRS{4}}
-@crscode EPSG{8249} LatLon{NAD83CSRS{5}}
-@crscode EPSG{8252} LatLon{NAD83CSRS{6}}
-@crscode EPSG{8255} LatLon{NAD83CSRS{7}}
-@crscode EPSG{9777} LatLon{RGF93v2}
-@crscode EPSG{9782} LatLon{RGF93v2b}
-@crscode EPSG{9988} Cartesian3D{ITRF{2020}}
-@crscode EPSG{10176} Cartesian3D{IGS20}
-@crscode EPSG{10414} LatLon{NAD83CSRS{8}}
-@crscode EPSG{25832} shift(TransverseMercator{0.9996,0.0°,ETRFLatest}, lonₒ=9.0°, xₒ=500000.0m, yₒ=0.0m)
-@crscode EPSG{27700} shift(TransverseMercator{0.9996012717,49.0°,OSGB36}, lonₒ=-2.0°, xₒ=400000.0m, yₒ=-100000.0m)
-@crscode EPSG{28355} shift(TransverseMercator{0.9996,0.0°,GDA94}, lonₒ=147.0°, xₒ=500000.0m, yₒ=10000000.0m)
-@crscode EPSG{29903} shift(TransverseMercator{1.000035,53.5°,Ire65}, lonₒ=-8.0°, xₒ=200000.0m, yₒ=250000.0m)
+) EPSG{2039}
+@crscode shift(TransverseMercator{0.99982,53.5°,IRENET95}, lonₒ=-8.0°, xₒ=600000.0m, yₒ=750000.0m) EPSG{2157}
+@crscode shift(TransverseMercator{0.9993,0.0°,ETRF{2000}}, lonₒ=19.0°, xₒ=500000.0m, yₒ=-5300000.0m) EPSG{2180}
+@crscode shift(TransverseMercator{0.9996,0.0°,NZGD2000}, lonₒ=173.0°, xₒ=1600000.0m, yₒ=10000000.0m) EPSG{2193}
+@crscode shift(LambertAzimuthal{52.0°,ETRFLatest}, lonₒ=10.0°, xₒ=4321000.0m, yₒ=3210000.0m) EPSG{3035}
+@crscode shift(Albers{0.0°,34.0°,40.5°,NAD83}, lonₒ=-120.0°, yₒ=-4000000.0m) EPSG{3310}
+@crscode shift(Albers{50.0°,55.0°,65.0°,NAD83}, lonₒ=-154.0°) EPSG{3338}
+@crscode Mercator{WGS84Latest} EPSG{3395}
+@crscode WebMercator{WGS84Latest} EPSG{3857}
+@crscode LatLon{RGF93v1} EPSG{4171}
+@crscode LatLon{Lisbon1937} EPSG{4207}
+@crscode LatLon{Aratu} EPSG{4208}
+@crscode LatLon{ED50} EPSG{4230}
+@crscode LatLon{ED87} EPSG{4231}
+@crscode LatLon{NAD27} EPSG{4267}
+@crscode LatLon{NAD83} EPSG{4269}
+@crscode LatLon{Datum73} EPSG{4274}
+@crscode LatLon{NTF} EPSG{4275}
+@crscode LatLon{OSGB36} EPSG{4277}
+@crscode LatLon{DHDN} EPSG{4314}
+@crscode LatLon{WGS84Latest} EPSG{4326}
+@crscode LatLon{SAD69} EPSG{4618}
+@crscode LatLon{ISN93} EPSG{4659}
+@crscode LatLon{Lisbon1890} EPSG{4666}
+@crscode LatLon{ED79} EPSG{4668}
+@crscode LatLon{SIRGAS2000} EPSG{4674}
+@crscode LatLon{RD83} EPSG{4745}
+@crscode LatLon{PD83} EPSG{4746}
+@crscode Cartesian3D{shift(ITRF{2000}, 2000.4)} EPSG{4988}
+@crscode LatLonAlt{shift(ITRF{2000}, 2000.4)} EPSG{4989}
+@crscode shift(Albers{23.0°,29.5°,45.5°,NAD83}, lonₒ=-96.0°) EPSG{5070}
+@crscode LatLon{ISN2004} EPSG{5324}
+@crscode LatLon{SAD96} EPSG{5527}
+@crscode LatLon{ISN2016} EPSG{8086}
+@crscode LatLon{NAD83CSRS{1}} EPSG{8232}
+@crscode LatLon{NAD83CSRS{2}} EPSG{8237}
+@crscode LatLon{NAD83CSRS{3}} EPSG{8240}
+@crscode LatLon{NAD83CSRS{4}} EPSG{8246}
+@crscode LatLon{NAD83CSRS{5}} EPSG{8249}
+@crscode LatLon{NAD83CSRS{6}} EPSG{8252}
+@crscode LatLon{NAD83CSRS{7}} EPSG{8255}
+@crscode LatLon{RGF93v2} EPSG{9777}
+@crscode LatLon{RGF93v2b} EPSG{9782}
+@crscode Cartesian3D{ITRF{2020}} EPSG{9988}
+@crscode Cartesian3D{IGS20} EPSG{10176}
+@crscode LatLon{NAD83CSRS{8}} EPSG{10414}
+@crscode shift(TransverseMercator{0.9996,0.0°,ETRFLatest}, lonₒ=9.0°, xₒ=500000.0m, yₒ=0.0m) EPSG{25832}
+@crscode shift(TransverseMercator{0.9996012717,49.0°,OSGB36}, lonₒ=-2.0°, xₒ=400000.0m, yₒ=-100000.0m) EPSG{27700}
+@crscode shift(TransverseMercator{0.9996,0.0°,GDA94}, lonₒ=147.0°, xₒ=500000.0m, yₒ=10000000.0m) EPSG{28355}
+@crscode shift(TransverseMercator{1.000035,53.5°,Ire65}, lonₒ=-8.0°, xₒ=200000.0m, yₒ=250000.0m) EPSG{29903}
 # For EPSG:31370, the lat/lon projection parameters are given in sexagesimal DMS in the EPSG database.
 # To avoid unit conversion, the literal parameters are those obtained with the PROJ command `projinfo EPSG:31370`.
-@crscode EPSG{31370} shift(
+@crscode shift(
   LambertConic{90.0°,51.1666672333333°,49.8333339°,BD72},
   lonₒ=4.36748666666667°,
   xₒ=150000.013m,
   yₒ=5400088.438m
-)
-@crscode EPSG{32662} PlateCarree{WGS84Latest}
-@crscode ESRI{54017} Behrmann{WGS84Latest}
-@crscode ESRI{54030} Robinson{WGS84Latest}
-@crscode ESRI{54034} LambertCylindrical{WGS84Latest}
-@crscode ESRI{54042} WinkelTripel{WGS84Latest}
-@crscode ESRI{102035} Orthographic{SphericalMode,90°,WGS84Latest}
-@crscode ESRI{102037} Orthographic{SphericalMode,-90°,WGS84Latest}
+) EPSG{31370}
+@crscode PlateCarree{WGS84Latest} EPSG{32662}
+@crscode Behrmann{WGS84Latest} ESRI{54017}
+@crscode Robinson{WGS84Latest} ESRI{54030}
+@crscode LambertCylindrical{WGS84Latest} ESRI{54034}
+@crscode WinkelTripel{WGS84Latest} ESRI{54042}
+@crscode Orthographic{SphericalMode,90°,WGS84Latest} ESRI{102035}
+@crscode Orthographic{SphericalMode,-90°,WGS84Latest} ESRI{102037}
 
 for zone in 1:60
   NorthCode = 32600 + zone
   SouthCode = 32700 + zone
-  @eval @crscode EPSG{$NorthCode} utmnorth($zone, datum=WGS84Latest)
-  @eval @crscode EPSG{$SouthCode} utmsouth($zone, datum=WGS84Latest)
+  @eval @crscode utmnorth($zone, datum=WGS84Latest) EPSG{$NorthCode}
+  @eval @crscode utmsouth($zone, datum=WGS84Latest) EPSG{$SouthCode}
 end
--- test/get.jl
+++ test/get.jl
@@ -1,109 +1,109 @@
 @testset "get" begin
   # EPSG/ESRI code
   gettest(
-    EPSG{2039},
     CoordRefSystems.shift(
       TransverseMercator{1.0000067,31.734394°,Israel1993},
       lonₒ=35.204517°,
       xₒ=219529.584m,
       yₒ=626907.39m
-    )
+    ),
+    EPSG{2039}
   )
   gettest(
-    EPSG{2157},
-    CoordRefSystems.shift(TransverseMercator{0.99982,53.5°,IRENET95}, lonₒ=-8.0°, xₒ=600000.0m, yₒ=750000.0m)
+    CoordRefSystems.shift(TransverseMercator{0.99982,53.5°,IRENET95}, lonₒ=-8.0°, xₒ=600000.0m, yₒ=750000.0m),
+    EPSG{2157}
   )
   gettest(
-    EPSG{2180},
-    CoordRefSystems.shift(TransverseMercator{0.9993,0.0°,ETRF{2000}}, lonₒ=19.0°, xₒ=500000.0m, yₒ=-5300000.0m)
+    CoordRefSystems.shift(TransverseMercator{0.9993,0.0°,ETRF{2000}}, lonₒ=19.0°, xₒ=500000.0m, yₒ=-5300000.0m),
+    EPSG{2180}
   )
   gettest(
-    EPSG{2193},
-    CoordRefSystems.shift(TransverseMercator{0.9996,0.0°,NZGD2000}, lonₒ=173.0°, xₒ=1600000.0m, yₒ=10000000.0m)
+    CoordRefSystems.shift(TransverseMercator{0.9996,0.0°,NZGD2000}, lonₒ=173.0°, xₒ=1600000.0m, yₒ=10000000.0m),
+    EPSG{2193}
   )
   gettest(
-    EPSG{3035},
-    CoordRefSystems.shift(LambertAzimuthal{52.0°,ETRFLatest}, lonₒ=10.0°, xₒ=4321000.0m, yₒ=3210000.0m)
+    CoordRefSystems.shift(LambertAzimuthal{52.0°,ETRFLatest}, lonₒ=10.0°, xₒ=4321000.0m, yₒ=3210000.0m),
+    EPSG{3035}
   )
-  gettest(EPSG{3310}, CoordRefSystems.shift(Albers{0.0°,34.0°,40.5°,NAD83}, lonₒ=-120.0°, yₒ=-4000000.0m))
-  gettest(EPSG{3338}, CoordRefSystems.shift(Albers{50.0°,55.0°,65.0°,NAD83}, lonₒ=-154.0°))
-  gettest(EPSG{3395}, Mercator{WGS84Latest})
-  gettest(EPSG{3857}, WebMercator{WGS84Latest})
-  gettest(EPSG{4171}, LatLon{RGF93v1})
-  gettest(EPSG{4207}, LatLon{Lisbon1937})
-  gettest(EPSG{4208}, LatLon{Aratu})
-  gettest(EPSG{4230}, LatLon{ED50})
-  gettest(EPSG{4231}, LatLon{ED87})
-  gettest(EPSG{4267}, LatLon{NAD27})
-  gettest(EPSG{4269}, LatLon{NAD83})
-  gettest(EPSG{4274}, LatLon{Datum73})
-  gettest(EPSG{4275}, LatLon{NTF})
-  gettest(EPSG{4277}, LatLon{OSGB36})
-  gettest(EPSG{4314}, LatLon{DHDN})
-  gettest(EPSG{4326}, LatLon{WGS84Latest})
-  gettest(EPSG{4618}, LatLon{SAD69})
-  gettest(EPSG{4659}, LatLon{ISN93})
-  gettest(EPSG{4666}, LatLon{Lisbon1890})
-  gettest(EPSG{4668}, LatLon{ED79})
-  gettest(EPSG{4674}, LatLon{SIRGAS2000})
-  gettest(EPSG{4745}, LatLon{RD83})
-  gettest(EPSG{4746}, LatLon{PD83})
-  gettest(EPSG{4988}, Cartesian{CoordRefSystems.shift(ITRF{2000}, 2000.4),3})
-  gettest(EPSG{4989}, LatLonAlt{CoordRefSystems.shift(ITRF{2000}, 2000.4)})
-  gettest(EPSG{5070}, CoordRefSystems.shift(Albers{23.0°,29.5°,45.5°,NAD83}, lonₒ=-96.0°))
-  gettest(EPSG{5324}, LatLon{ISN2004})
-  gettest(EPSG{5527}, LatLon{SAD96})
-  gettest(EPSG{8086}, LatLon{ISN2016})
-  gettest(EPSG{8232}, LatLon{NAD83CSRS{1}})
-  gettest(EPSG{8237}, LatLon{NAD83CSRS{2}})
-  gettest(EPSG{8240}, LatLon{NAD83CSRS{3}})
-  gettest(EPSG{8246}, LatLon{NAD83CSRS{4}})
-  gettest(EPSG{8249}, LatLon{NAD83CSRS{5}})
-  gettest(EPSG{8252}, LatLon{NAD83CSRS{6}})
-  gettest(EPSG{8255}, LatLon{NAD83CSRS{7}})
-  gettest(EPSG{9777}, LatLon{RGF93v2})
-  gettest(EPSG{9782}, LatLon{RGF93v2b})
-  gettest(EPSG{9988}, Cartesian{ITRF{2020},3})
-  gettest(EPSG{10176}, Cartesian{IGS20,3})
-  gettest(EPSG{10414}, LatLon{NAD83CSRS{8}})
+  gettest(CoordRefSystems.shift(Albers{0.0°,34.0°,40.5°,NAD83}, lonₒ=-120.0°, yₒ=-4000000.0m), EPSG{3310})
+  gettest(CoordRefSystems.shift(Albers{50.0°,55.0°,65.0°,NAD83}, lonₒ=-154.0°), EPSG{3338})
+  gettest(Mercator{WGS84Latest}, EPSG{3395})
+  gettest(WebMercator{WGS84Latest}, EPSG{3857})
+  gettest(LatLon{RGF93v1}, EPSG{4171})
+  gettest(LatLon{Lisbon1937}, EPSG{4207})
+  gettest(LatLon{Aratu}, EPSG{4208})
+  gettest(LatLon{ED50}, EPSG{4230})
+  gettest(LatLon{ED87}, EPSG{4231})
+  gettest(LatLon{NAD27}, EPSG{4267})
+  gettest(LatLon{NAD83}, EPSG{4269})
+  gettest(LatLon{Datum73}, EPSG{4274})
+  gettest(LatLon{NTF}, EPSG{4275})
+  gettest(LatLon{OSGB36}, EPSG{4277})
+  gettest(LatLon{DHDN}, EPSG{4314})
+  gettest(LatLon{WGS84Latest}, EPSG{4326})
+  gettest(LatLon{SAD69}, EPSG{4618})
+  gettest(LatLon{ISN93}, EPSG{4659})
+  gettest(LatLon{Lisbon1890}, EPSG{4666})
+  gettest(LatLon{ED79}, EPSG{4668})
+  gettest(LatLon{SIRGAS2000}, EPSG{4674})
+  gettest(LatLon{RD83}, EPSG{4745})
+  gettest(LatLon{PD83}, EPSG{4746})
+  gettest(Cartesian{CoordRefSystems.shift(ITRF{2000}, 2000.4),3}, EPSG{4988})
+  gettest(LatLonAlt{CoordRefSystems.shift(ITRF{2000}, 2000.4)}, EPSG{4989})
+  gettest(CoordRefSystems.shift(Albers{23.0°,29.5°,45.5°,NAD83}, lonₒ=-96.0°), EPSG{5070})
+  gettest(LatLon{ISN2004}, EPSG{5324})
+  gettest(LatLon{SAD96}, EPSG{5527})
+  gettest(LatLon{ISN2016}, EPSG{8086})
+  gettest(LatLon{NAD83CSRS{1}}, EPSG{8232})
+  gettest(LatLon{NAD83CSRS{2}}, EPSG{8237})
+  gettest(LatLon{NAD83CSRS{3}}, EPSG{8240})
+  gettest(LatLon{NAD83CSRS{4}}, EPSG{8246})
+  gettest(LatLon{NAD83CSRS{5}}, EPSG{8249})
+  gettest(LatLon{NAD83CSRS{6}}, EPSG{8252})
+  gettest(LatLon{NAD83CSRS{7}}, EPSG{8255})
+  gettest(LatLon{RGF93v2}, EPSG{9777})
+  gettest(LatLon{RGF93v2b}, EPSG{9782})
+  gettest(Cartesian{ITRF{2020},3}, EPSG{9988})
+  gettest(Cartesian{IGS20,3}, EPSG{10176})
+  gettest(LatLon{NAD83CSRS{8}}, EPSG{10414})
   gettest(
-    EPSG{25832},
-    CoordRefSystems.shift(TransverseMercator{0.9996,0.0°,ETRFLatest}, lonₒ=9.0°, xₒ=500000.0m, yₒ=0.0m)
-  )
-  gettest(
-    EPSG{27700},
-    CoordRefSystems.shift(TransverseMercator{0.9996012717,49.0°,OSGB36}, lonₒ=-2.0°, xₒ=400000.0m, yₒ=-100000.0m)
+    CoordRefSystems.shift(TransverseMercator{0.9996,0.0°,ETRFLatest}, lonₒ=9.0°, xₒ=500000.0m, yₒ=0.0m),
+    EPSG{25832}
   )
   gettest(
-    EPSG{28355},
-    CoordRefSystems.shift(TransverseMercator{0.9996,0.0°,GDA94}, lonₒ=147.0°, xₒ=500000.0m, yₒ=10000000.0m)
+    CoordRefSystems.shift(TransverseMercator{0.9996012717,49.0°,OSGB36}, lonₒ=-2.0°, xₒ=400000.0m, yₒ=-100000.0m),
+    EPSG{27700}
   )
   gettest(
-    EPSG{29903},
-    CoordRefSystems.shift(TransverseMercator{1.000035,53.5°,Ire65}, lonₒ=-8.0°, xₒ=200000.0m, yₒ=250000.0m)
+    CoordRefSystems.shift(TransverseMercator{0.9996,0.0°,GDA94}, lonₒ=147.0°, xₒ=500000.0m, yₒ=10000000.0m),
+    EPSG{28355}
   )
   gettest(
-    EPSG{31370},
+    CoordRefSystems.shift(TransverseMercator{1.000035,53.5°,Ire65}, lonₒ=-8.0°, xₒ=200000.0m, yₒ=250000.0m),
+    EPSG{29903}
+  )
+  gettest(
     CoordRefSystems.shift(
       LambertConic{90.0°,51.1666672333333°,49.8333339°,BD72},
       lonₒ=4.36748666666667°,
       xₒ=150000.013m,
       yₒ=5400088.438m
-    )
+    ),
+    EPSG{31370}
   )
-  gettest(EPSG{32662}, PlateCarree{WGS84Latest})
-  gettest(ESRI{54017}, Behrmann{WGS84Latest})
-  gettest(ESRI{54030}, Robinson{WGS84Latest})
-  gettest(ESRI{54034}, LambertCylindrical{WGS84Latest})
-  gettest(ESRI{54042}, WinkelTripel{WGS84Latest})
-  gettest(ESRI{102035}, CoordRefSystems.Orthographic{CoordRefSystems.SphericalMode,90°,WGS84Latest})
-  gettest(ESRI{102037}, CoordRefSystems.Orthographic{CoordRefSystems.SphericalMode,-90°,WGS84Latest})
+  gettest(PlateCarree{WGS84Latest}, EPSG{32662})
+  gettest(Behrmann{WGS84Latest}, ESRI{54017})
+  gettest(Robinson{WGS84Latest}, ESRI{54030})
+  gettest(LambertCylindrical{WGS84Latest}, ESRI{54034})
+  gettest(WinkelTripel{WGS84Latest}, ESRI{54042})
+  gettest(CoordRefSystems.Orthographic{CoordRefSystems.SphericalMode,90°,WGS84Latest}, ESRI{102035})
+  gettest(CoordRefSystems.Orthographic{CoordRefSystems.SphericalMode,-90°,WGS84Latest}, ESRI{102037})
 
   for zone in 1:60
     NorthCode = 32600 + zone
     SouthCode = 32700 + zone
-    gettest(EPSG{NorthCode}, utmnorth(zone, datum=WGS84Latest))
-    gettest(EPSG{SouthCode}, utmsouth(zone, datum=WGS84Latest))
+    gettest(utmnorth(zone, datum=WGS84Latest), EPSG{NorthCode})
+    gettest(utmsouth(zone, datum=WGS84Latest), EPSG{SouthCode})
   end
 
   # CRS string
