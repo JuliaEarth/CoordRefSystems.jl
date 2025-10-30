27b48bc3c07e76150d608d7998c385267ac46b20
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-10-30 14:35:03 -0300

    Update src/strings.jl
    Co-authored-by: Elias Carvalho <73039601+eliascarv@users.noreply.github.com>

--- src/strings.jl
+++ src/strings.jl
@@ -5,82 +5,75 @@
 # if you're coming from an error message,
 # please add an entry to the following
 # dictionary in **alphabetical order**
-const esriid2code = let
-  d = Dict(
-    "British_National_Grid" => EPSG{27700},
-    "ETRF2000-PL_CS92" => EPSG{2180},
-    "ETRS_1989_UTM_Zone_32N" => EPSG{25832},
-    "ETRS_1989_LAEA" => EPSG{3035},
-    "GCS_Datum_73" => EPSG{4274},
-    "GCS_Deutsches_Hauptdreiecksnetz" => EPSG{4314},
-    "GCS_European_1950" => EPSG{4230},
-    "GCS_European_1987" => EPSG{4231},
-    "GCS_European_1979" => EPSG{4668},
-    "GCS_ISN_1993" => EPSG{4659},
-    "GCS_ISN_2004" => EPSG{5324},
-    "GCS_ISN_2016" => EPSG{8086},
-    "GCS_Lisbon" => EPSG{4207},
-    "GCS_Lisbon_1890" => EPSG{4666},
-    "GCS_MAGNA" => EPSG{4686},
-    "GCS_NAD83_CSRS96" => EPSG{8232},
-    "GCS_NAD83_CSRS_v2" => EPSG{8237},
-    "GCS_NAD83_CSRS_v3" => EPSG{8240},
-    "GCS_NAD83_CSRS_v4" => EPSG{8246},
-    "GCS_NAD83_CSRS_v5" => EPSG{8249},
-    "GCS_NAD83_CSRS_v6" => EPSG{8252},
-    "GCS_NAD83_CSRS_v7" => EPSG{8255},
-    "GCS_NAD83_CSRS_v8" => EPSG{10414},
-    "GCS_North_American_1927" => EPSG{4267},
-    "GCS_North_American_1983" => EPSG{4269},
-    "GCS_NTF" => EPSG{4275},
-    "GCS_OSGB_1936" => EPSG{4277},
-    "GCS_PD/83" => EPSG{4746},
-    "GCS_RD/83" => EPSG{4745},
-    "GCS_RGF_1993" => EPSG{4171},
-    "GCS_SAD_1969_96" => EPSG{5527},
-    "GCS_SIRGAS_2000" => EPSG{4674},
-    "GCS_South_American_1969" => EPSG{4618},
-    "GCS_WGS_1984" => EPSG{4326},
-    "IRENET95_Irish_Transverse_Mercator" => EPSG{2157},
-    "NAD_1983_California_Teale_Albers" => EPSG{3310},
-    "NAD_1983_Contiguous_USA_Albers" => EPSG{5070},
-    "North_Pole_Orthographic" => ESRI{102035},
-    "NZGD_2000_New_Zealand_Transverse_Mercator" => EPSG{2193},
-    "RGF93_v2" => EPSG{9777},
-    "RGF93_v2b" => EPSG{9782},
-    "SIRGAS_2000_UTM_Zone_23N" => EPSG{6210},
-    "SIRGAS_2000_UTM_Zone_24N" => EPSG{6211},
-    "SIRGAS_2000_UTM_Zone_26S" => EPSG{5396},
-    "South_Pole_Orthographic" => ESRI{102037},
-    "TM75_Irish_Grid" => EPSG{29903},
-    "WGS_1984_Plate_Carree" => EPSG{32662},
-    "WGS_1984_UTM_Zone_33N" => EPSG{32633},
-    "WGS_1984_Web_Mercator_Auxiliary_Sphere" => EPSG{3857},
-    "WGS_1984_World_Mercator" => EPSG{3395},
-    "World_Behrmann" => ESRI{54017},
-    "World_Cylindrical_Equal_Area" => ESRI{54034},
-    "World_Robinson" => ESRI{54030},
-    "World_Winkel_Tripel_NGS" => ESRI{54042}
-  )
+const esriid2code = Dict(
+  "British_National_Grid" => EPSG{27700},
+  "ETRF2000-PL_CS92" => EPSG{2180},
+  "ETRS_1989_UTM_Zone_32N" => EPSG{25832},
+  "ETRS_1989_LAEA" => EPSG{3035},
+  "GCS_Datum_73" => EPSG{4274},
+  "GCS_Deutsches_Hauptdreiecksnetz" => EPSG{4314},
+  "GCS_European_1950" => EPSG{4230},
+  "GCS_European_1987" => EPSG{4231},
+  "GCS_European_1979" => EPSG{4668},
+  "GCS_ISN_1993" => EPSG{4659},
+  "GCS_ISN_2004" => EPSG{5324},
+  "GCS_ISN_2016" => EPSG{8086},
+  "GCS_Lisbon" => EPSG{4207},
+  "GCS_Lisbon_1890" => EPSG{4666},
+  "GCS_MAGNA" => EPSG{4686},
+  "GCS_NAD83_CSRS96" => EPSG{8232},
+  "GCS_NAD83_CSRS_v2" => EPSG{8237},
+  "GCS_NAD83_CSRS_v3" => EPSG{8240},
+  "GCS_NAD83_CSRS_v4" => EPSG{8246},
+  "GCS_NAD83_CSRS_v5" => EPSG{8249},
+  "GCS_NAD83_CSRS_v6" => EPSG{8252},
+  "GCS_NAD83_CSRS_v7" => EPSG{8255},
+  "GCS_NAD83_CSRS_v8" => EPSG{10414},
+  "GCS_North_American_1927" => EPSG{4267},
+  "GCS_North_American_1983" => EPSG{4269},
+  "GCS_NTF" => EPSG{4275},
+  "GCS_OSGB_1936" => EPSG{4277},
+  "GCS_PD/83" => EPSG{4746},
+  "GCS_RD/83" => EPSG{4745},
+  "GCS_RGF_1993" => EPSG{4171},
+  "GCS_SAD_1969_96" => EPSG{5527},
+  "GCS_SIRGAS_2000" => EPSG{4674},
+  "GCS_South_American_1969" => EPSG{4618},
+  "GCS_WGS_1984" => EPSG{4326},
+  "IRENET95_Irish_Transverse_Mercator" => EPSG{2157},
+  "NAD_1983_California_Teale_Albers" => EPSG{3310},
+  "NAD_1983_Contiguous_USA_Albers" => EPSG{5070},
+  "North_Pole_Orthographic" => ESRI{102035},
+  "NZGD_2000_New_Zealand_Transverse_Mercator" => EPSG{2193},
+  "RGF93_v2" => EPSG{9777},
+  "RGF93_v2b" => EPSG{9782},
+  "South_Pole_Orthographic" => ESRI{102037},
+  "TM75_Irish_Grid" => EPSG{29903},
+  "WGS_1984_Plate_Carree" => EPSG{32662},
+  "WGS_1984_UTM_Zone_33N" => EPSG{32633},
+  "WGS_1984_Web_Mercator_Auxiliary_Sphere" => EPSG{3857},
+  "WGS_1984_World_Mercator" => EPSG{3395},
+  "World_Behrmann" => ESRI{54017},
+  "World_Cylindrical_Equal_Area" => ESRI{54034},
+  "World_Robinson" => ESRI{54030},
+  "World_Winkel_Tripel_NGS" => ESRI{54042}
+)
 
-  for zone in 1:60
-    NorthCode = 32600 + zone
-    SouthCode = 32700 + zone
-    d["WGS_1984_UTM_Zone_$(zone)N"] = EPSG{NorthCode}
-    d["WGS_1984_UTM_Zone_$(zone)S"] = EPSG{SouthCode}
-  end
+for zone in 1:60
+  NorthCode = 32600 + zone
+  SouthCode = 32700 + zone
+  esriid2code["WGS_1984_UTM_Zone_$(zone)N"] = EPSG{NorthCode}
+  esriid2code["WGS_1984_UTM_Zone_$(zone)S"] = EPSG{SouthCode}
+end
 
-  for zone in 11:22
-    NorthCode = 31954 + zone
-    d["SIRGAS_2000_UTM_Zone_$(zone)N"] = EPSG{NorthCode}
-  end
+for zone in 11:22
+  NorthCode = 31954 + zone
+  esriid2code["SIRGAS_2000_UTM_Zone_$(zone)N"] = EPSG{NorthCode}
+end
 
-  for zone in 17:25
-    SouthCode = 31960 + zone
-    d["SIRGAS_2000_UTM_Zone_$(zone)S"] = EPSG{SouthCode}
-  end
-
-  d
+for zone in 17:25
+  SouthCode = 31960 + zone
+  esriid2code["SIRGAS_2000_UTM_Zone_$(zone)S"] = EPSG{SouthCode}
 end
 
 """
